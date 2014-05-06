ce2268f072a2b9097f71a4e81b88c149e31e7e11
Taizo Sadhiro <sadahiro@taizox201.(none)>
2014-05-06 15:47:58 +0900

    add patience game r script

---
+++ patience.r
@@ -0,0 +1,26 @@
+
+patiencegame <- function(n){
+    deck <- sample(1:n)
+    piles <- list()
+    tops <- c()
+
+    for (k in deck){
+        if (sum(tops > k) == 0){
+            piles <- append(piles, k)
+            tops <- c(tops,k)
+        }else{
+            for (i in order(tops)){
+                if (tops[i] > k){
+                    tops[i] <- k
+                    piles[[i]] <- c(piles[[i]],k)
+                    break
+                }
+            }
+        }
+    }
+
+    return(list(deck=deck,piles=piles))
+}
+
+
+
